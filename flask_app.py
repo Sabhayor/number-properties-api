ddac2293d63e2222166e2ad6bbd8abd582b0fd15
sabhayor <ayodeji.saberedowo@norrenberger.local>
2025-02-07 22:22:30 +0100

    CORS enabled

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-#from flask_cors import CORS
+from flask_cors import CORS
 import math
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 
 def is_prime(n):
     if n <= 1:
